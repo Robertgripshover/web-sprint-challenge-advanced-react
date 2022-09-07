1a8c9f949a58b4326eaa2d11adb53dae0fa95434
Robert Gripshover <robertgrips@gmail.com>
2022-09-07 09:07:27 -0400

    m

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -5,18 +5,8 @@ import React from 'react'
   //you just need to add 1 to the index. BUT to go up or down, you need to add 4 to the index, 
   //becuase that is going to make it 'jump' down the line. VERY INTERESTING! 
 
-// Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+//9-6-22 declaring these variables to hopefully be able to call on them below
 
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-}
 
 export default class AppClass extends React.Component {
 
@@ -24,37 +14,12 @@ export default class AppClass extends React.Component {
     indexOfB: 4, 
     coordinates: '',
     message: '', 
-    board: ['', '', '', '', 'B', '', '', '', ''],
+    board: ['', '', '', '', '', '', '', '', ''],
     totalMoves: 0,
     direction: ''
   } 
 
-handleClick = (direction) => {
-  if(direction = 'up') {return indexOfB - 4}
-  if(direction = 'down') {return indexOfB + 4}
-  if(direction = 'left') {return indexOfB - 1}
-  if(direction = 'right') {return indexOfB + 1}
-} //9-6-22 I need to try and make sure to make the indexOfB stateful
-
-
-  // //this function is not working yet VVV
-  // updateIndexOfB = (board, indexOfB) => {
-  //    board.map((index) => {
-  //     if(index  === 0 && index === 'B') {return indexOfB = 0}
-  //     else if(index  === 1 && index === 'B') {return indexOfB = 1}
-  //     else if(index  === 2 && index === 'B') {return indexOfB = 2}
-  //     else if(index  === 3 && index === 'B') {return indexOfB = 3}
-  //     else if(index  === 4 && index === 'B') {return indexOfB = 4}
-  //     else if(index  === 5 && index === 'B') {return indexOfB = 5}
-  //     else if(index  === 6 && index === 'B') {return indexOfB = 6}
-  //     else if(index  === 7 && index === 'B') {return indexOfB = 7}
-  //     else if(index  === 8 && index === 'B') {return indexOfB = 8}      
-  //    }) 
-
-  // } //trying to make this function take the current index in the 'B' is at in the 'board'
-  // //and then use that to update the indexOfB
-
-  //this VVV getXY function is working!!
+  //this VVV getXY function is working
   getXY = (indexOfB, coordinates) => {
     if (indexOfB === 0) {return coordinates = "(1, 3)"}
     if (indexOfB === 1) {return coordinates = "(2, 3)"}
@@ -70,6 +35,9 @@ handleClick = (direction) => {
   //this ^^^ getXY function is working!!
   //this above function is looking at what the indexOfB is and simply updating the coordinates accordingly
 
+  handleTurn = (idx) => {
+    console.log(idx)
+  }
   
   reset = () => {
     this.setState({
@@ -85,37 +53,15 @@ handleClick = (direction) => {
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
 
-  getDirection = (direction, indexOfB, message) => {
-    if (direction === 'right' && indexOfB < 7) {return indexOfB + 1}
-    if (direction === 'right' && indexOfB === 8) {return message = 'You cannot move right more'}
-    if (direction === 'left' && indexOfB > 1) {return indexOfB - 1}
-    if (direction === 'left' && indexOfB === 0) {return message = 'You cannot move left more'}
-    if (direction === 'up' && indexOfB > 4) {return indexOfB - 4}
-    if (direction === 'up' && indexOfB < 4) {return message = 'You cannot move up more'}
-    if (direction === 'down' && indexOfB < 4) {return indexOfB + 4}
-    if (direction === 'down' && indexOfB > 4) {return message = 'You cannot move down more'}
+  getDirection = (direction) => {
+    if (direction = 'left') { return idx - 1}
+    if (direction = 'right') { return idx + 1}  
+    if (direction = 'down') { return idx + 4}  
+    if (direction = 'up') { return idx - 4}    
   }
 
-  goRight = () => {
-    this.getDirection()
-    return console.log('moving right!')
-  }
-
-  goLeft = () => {
-    return console.log('moving left!')
-  }
-
-  goUp = () => {
-    return console.log('moving up!')
-  }
-
-  goDown = () => {
-    return console.log('moving down!')
-  }
-
-  move = (evt) => {
-
-    // This event handler can use the helper above to obtain a new index for the "B",
+   move = (evt) => {
+   // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
   onChange = (evt) => {
@@ -152,11 +98,11 @@ handleClick = (direction) => {
           <h3 id="message">{`${this.getDirection(message)}`}</h3> {/*for some reason at the moment this message is just returning 'undefined' */}
         </div>
         <div id="keypad">
-          <button onClick={() => this.handleClick()} id="left">LEFT</button>
-          <button  id="up">UP</button>
-          <button  id="right">RIGHT</button>
-          <button  id="down">DOWN</button>
-          <button  id="reset">reset</button>
+          <button id="left">LEFT</button>
+          <button id="up">UP</button>
+          <button id="right">RIGHT</button>
+          <button id="down">DOWN</button>
+          <button id="reset">reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
