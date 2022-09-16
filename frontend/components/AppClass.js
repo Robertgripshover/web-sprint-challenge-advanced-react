d2d645e23f45746c003e9a711241033f778743ac
Robert Gripshover <robertgrips@gmail.com>
2022-09-16 14:31:59 -0400

    m

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,56 +1,69 @@
 import React from 'react'
-
-//NOTES:::
-  //So just a little note for myself, I think that for moving the index of B left or right 
-  //you just need to add 1 to the index. BUT to go up or down, you need to add 4 to the index, 
-  //becuase that is going to make it 'jump' down the line. VERY INTERESTING! 
-
-//9-6-22 declaring these variables to hopefully be able to call on them below
-
+import arrayMove from 'array.prototype.move'
 
 export default class AppClass extends React.Component {
 
   constructor() {
     super()
     this.state = {
-      indexOfB: 4, 
+      indexOfB: 0, 
       coordinates: '',
       message: '', 
-      board: ['', '', '', '', '', '', '', '', ''],
+      board: ['', '', 'B', '', '', '', '', '', ''],
       totalMoves: 0,
-      direction: ''
+      direction: '',
+      emailInput: '',
+      xCoordinate: 0,
+      yCoordinate: 0
     }
   }
 
-  // state = {
-  //   indexOfB: 4, 
-  //   coordinates: '',
-  //   message: '', 
-  //   board: ['', '', '', '', '', '', '', '', ''],
-  //   totalMoves: 0,
-  //   direction: ''
-  // } 
+  getIndexOfB = () => {
+
+  }
+
+  moveRight = (indexOfB) => {
+    this.setState({ ...this.state, indexOfB: indexOfB + 1})
+    console.log(indexOfB)
+  }
 
   //this VVV getXY function is working
-  getXY = (indexOfB, coordinates) => {
-    if (indexOfB === 0) {return coordinates = "(1, 3)"}
-    if (indexOfB === 1) {return coordinates = "(2, 3)"}
-    if (indexOfB === 2) {return coordinates = "(3, 3)"}
-    if (indexOfB === 3) {return coordinates = "(1, 2)"}
-    if (indexOfB === 4) {return coordinates = "(2, 2)"}
-    if (indexOfB === 5) {return coordinates = "(3, 2)"}
-    if (indexOfB === 6) {return coordinates = "(1, 1)"}
-    if (indexOfB === 7) {return coordinates = "(2, 1)"}
-    if (indexOfB === 8) {return coordinates = "(3, 1)"}
-  }//this is looking at where the B is at and returning a string stating what part of the "grid"
-  //that the idx is at. I dont think that the If statement logic is set up correctly yet. 
-  //this ^^^ getXY function is working!!
-  //this above function is looking at what the indexOfB is and simply updating the coordinates accordingly
+  // getXY = (indexOfB, coordinates) => {
+  //   if (indexOfB === 0) {return coordinates = "(1, 1)"}
+  //   if (indexOfB === 1) {return coordinates = "(2, 1)"}
+  //   if (indexOfB === 2) {return coordinates = "(3, 1)"}
+  //   if (indexOfB === 3) {return coordinates = "(1, 2)"}
+  //   if (indexOfB === 4) {return coordinates = "(2, 2)"}
+  //   if (indexOfB === 5) {return coordinates = "(3, 2)"}
+  //   if (indexOfB === 6) {return coordinates = "(1, 3)"}
+  //   if (indexOfB === 7) {return coordinates = "(2, 3)"}
+  //   if (indexOfB === 8) {return coordinates = "(3, 3)"}
+  // } THIS FUNCTION IS WORKING! AFTER EXPERIMINTATTION JUST LET ME KNOW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  handleTurn = (idx) => {
-    console.log(idx)
+  getXY = (indexOfB, xCoordinate, yCoordinate) => {
+    if (indexOfB === 0) {return xCoordinate === 1 && yCoordinate === 1}
+    else if (indexOfB === 1) {return xCoordinate === 2 && yCoordinate === 1}
+    else if (indexOfB === 2) {return xCoordinate === 3 && yCoordinate === 1}
+    else if (indexOfB === 3) {return xCoordinate === 1 && yCoordinate === 2}
+    else if (indexOfB === 4) {return xCoordinate === 2 && yCoordinate === 2}
+    else if (indexOfB === 5) {return xCoordinate === 3 && yCoordinate === 2}
+    else if (indexOfB === 6) {return xCoordinate === 1 && yCoordinate === 3}
+    else if (indexOfB === 7) {return xCoordinate === 2 && yCoordinate === 3}
+    else if (indexOfB === 8) {return xCoordinate === 3 && yCoordinate === 3}
   }
-  
+
+  getXYMessage = (xCoordinate, yCoordinate, coordinates) => {
+    if (xCoordinate === 1 && yCoordinate === 1) {return coordinates = "(1, 1)"}
+    if (xCoordinate === 2 && yCoordinate === 1) {return coordinates = "(2, 1)"}
+    if (xCoordinate === 3 && yCoordinate === 1) {return coordinates = "(3, 1)"}
+    if (xCoordinate === 1 && yCoordinate === 2) {return coordinates = "(1, 2)"}
+    if (xCoordinate === 2 && yCoordinate === 2) {return coordinates = "(2, 2)"}
+    if (xCoordinate === 3 && yCoordinate === 2) {return coordinates = "(3, 2)"}
+    if (xCoordinate === 1 && yCoordinate === 3) {return coordinates = "(1, 3)"}
+    if (xCoordinate === 2 && yCoordinate === 3) {return coordinates = "(2, 3)"}
+    if (xCoordinate === 3 && yCoordinate === 3) {return coordinates = "(3, 3)"}
+  }
+
   reset = () => {
     this.setState({
       indexOfB: 4,  
@@ -65,23 +78,31 @@ export default class AppClass extends React.Component {
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
 
-  getDirection = (direction) => {
-    if (direction = 'left') { return idx - 1}
-    if (direction = 'right') { return idx + 1}  
-    if (direction = 'down') { return idx + 4}  
-    if (direction = 'up') { return idx - 4}    
+  getNextIndex = (direction) => {
+    if (direction === 'left') {return this.setState({...this.state, indexOfB: - 1})}
+    else if (direction === 'right') {return this.setState({...this.state, indexOfB: + 1})} 
+    else if (direction === 'down') {return this.setState({...this.state, indexOfB: + 4})} 
+    else if (direction === 'up') {return this.setState({...this.state, indexOfB: - 4})}  
   }
 
    move = (evt) => {
    // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+
   }
-  onChange = (evt) => {
+  onChangeOfEmail = (evt) => {
     // You will need this to update the value of the input.
+    const { value } = evt.target
+    this.setState({ ...this.state, emailInput: value })
   }
   onSubmit = (evt) => {
     // Use a POST request to send a payload to the server.
   }
+
+  moveBToTheRight = (evt) => {
+    evt.board.move(3, 4)
+  }
+
   render() {
 
     const { className } = this.props
@@ -107,17 +128,17 @@ export default class AppClass extends React.Component {
       </div>
 
         <div className="info">
-          <h3 id="message">{`${this.getDirection(message)}`}</h3> {/*for some reason at the moment this message is just returning 'undefined' */}
+          <h3 id="message">Need to get this working</h3>
         </div>
         <div id="keypad">
           <button id="left">LEFT</button>
           <button id="up">UP</button>
-          <button id="right">RIGHT</button>
+          <button onClick={this.moveBToTheRight} id="right">RIGHT</button>
           <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.reset} id="reset">reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
+          <input value={this.state.emailInput} onChange={this.onChangeOfEmail} id="email" type="email" placeholder="type email"></input> {/*working on this from the day2 solution video at minute 25 ALSO MORE INFO IN THE MODULE 3 GUIDED PROJECT*/} 
           <input id="submit" type="submit"></input>
         </form>
       </div>
