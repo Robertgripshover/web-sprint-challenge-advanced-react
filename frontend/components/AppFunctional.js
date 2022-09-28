4801525aa6f324e29c7f8099204b885936c439c7
Robert Gripshover <robertgrips@gmail.com>
2022-09-28 15:56:25 -0400

    got most of the class based working, and implimenting the functional

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -188,7 +188,7 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
  <div className="info">
-          <h3 id="coordinates">{`Coordinates ${getXY(indexOfB)}`}</h3>
+          <h3 id="coordinates">{`Coordinates ${getXY(state.indexOfB)}`}</h3>
           <h3 id="steps">You moved {state.totalMoves} times</h3>
         </div>
 
