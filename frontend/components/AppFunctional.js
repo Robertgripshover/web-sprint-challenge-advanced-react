dbd5bbd77e711ecf673fbb780004735cba1e9b47
Robert Gripshover <robertgrips@gmail.com>
2022-09-28 15:53:03 -0400

    finally got the up and down buttons to work!

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,82 +1,222 @@
 import React, {useState} from 'react'
 
-
-// Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
-
 export default function AppFunctional(props) {
 
-  const [state, setState] = useState()
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
+  const [state, setState] = useState({
+    coordinates: '',
+    message: '', 
+    board: ['', '', '', '', 'B', '', '', '', ''],
+    totalMoves: 0,
+    emailInput: '',
+    xCoordinate: 1,
+    yCoordinate: 1
+  })
 
-  function getXY() {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+  const getXY = (coordinates) => {
+    if (state.board.indexOf('B') === 0) {return coordinates = "(1, 1)"}
+    if (state.board.indexOf('B') === 1) {return coordinates = "(2, 1)"}
+    if (state.board.indexOf('B') === 2) {return coordinates = "(3, 1)"}
+    if (state.board.indexOf('B') === 3) {return coordinates = "(1, 2)"}
+    if (state.board.indexOf('B') === 4) {return coordinates = "(2, 2)"}
+    if (state.board.indexOf('B') === 5) {return coordinates = "(3, 2)"}
+    if (state.board.indexOf('B') === 6) {return coordinates = "(1, 3)"}
+    if (state.board.indexOf('B') === 7) {return coordinates = "(2, 3)"}
+    if (state.board.indexOf('B') === 8) {return coordinates = "(3, 3)"}
+  } 
+
+  const updateX = () => {
+    if (state.coordinates === "(1, 1)") {return setState({...state, yCoordinate: 1})}
+    if (state.coordinates === "(2, 1)") {return setState({...state, yCoordinate: 2})}
+    if (state.coordinates === "(3, 1)") {return setState({...state, yCoordinate: 3})}
+    if (state.coordinates === "(1, 2)") {return setState({...state, yCoordinate: 1})}
+    if (state.coordinates === "(2, 2)") {return setState({...state, yCoordinate: 2})}
+    if (state.coordinates === "(3, 2)") {return setState({...state, yCoordinate: 3})}
+    if (state.coordinates === "(1, 3)") {return setState({...state, yCoordinate: 1})}
+    if (state.coordinates === "(2, 3)") {return setState({...state, yCoordinate: 2})}
+    if (state.coordinates === "(3, 3)") {return setState({...state, yCoordinate: 3})}
   }
 
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+  const updateY = () => {
+    if (state.coordinates === "(1, 1)") {return setState({...state, yCoordinate: 1})}
+    if (state.coordinates === "(2, 1)") {return setState({...state, yCoordinate: 1})}
+    if (state.coordinates === "(3, 1)") {return setState({...state, yCoordinate: 1})}
+    if (state.coordinates === "(1, 2)") {return setState({...state, yCoordinate: 2})}
+    if (state.coordinates === "(2, 2)") {return setState({...state, yCoordinate: 2})}
+    if (state.coordinates === "(3, 2)") {return setState({...state, yCoordinate: 2})}
+    if (state.coordinates === "(1, 3)") {return setState({...state, yCoordinate: 3})}
+    if (state.coordinates === "(2, 3)") {return setState({...state, yCoordinate: 3})}
+    if (state.coordinates === "(3, 3)") {return setState({...state, yCoordinate: 3})}
   }
 
-  function reset() {
-    // Use this helper to reset all states to their initial values.
+  const reset = () => {
+    setState({
+      indexOfB: 4,  
+      message: '', 
+      board: ['', '', '', '', 'B', '', '', '', ''],
+      totalMoves: 0,
+      emailInput: '',
+      xCoordinate: 2,
+      yCoordinate: 2
+    })
+    console.log('resetting!')
   }
 
-  function getNextIndex(direction) {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+  const addToFrontOfArray = () => {
+    const newBoard = [...state.board]
+    newBoard.unshift(newBoard.pop())
+    setState({
+      ...state, 
+      board: newBoard
+    })
   }
 
-  function move(evt) {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+  const addToBackOfArray = () => {
+    const newBoard = [...state.board]
+    newBoard.push(newBoard.shift())
+    setState({
+      ...state, 
+      board: newBoard
+    })
   }
 
-  function onChange(evt) {
-    // You will need this to update the value of the input.
+  const removeThreeItemsFromFrontOfArray = () => {
+    setState({ board: state.board.slice(3)})
   }
 
-  function onSubmit(evt) {
-    // Use a POST request to send a payload to the server.
+  const moveBDownWithUnshiftAndPop = () => {
+    const newBoard = [...state.board]
+    newBoard.unshift(newBoard.pop(), newBoard.pop(), newBoard.pop())
+    setState({...state, board: newBoard})
   }
+
+  const moveBUpWithPushAndShift = () => {
+    const newBoard = [...state.board]
+    newBoard.push(newBoard.shift(), newBoard.shift(), newBoard.shift(),)
+    setState({...state, board: newBoard})
+  }
+
+  const removeItemFromFrontOfArray = () => {
+    setState({ board: state.board.slice(1)})
+  } 
+
+  const incrementTotalMoves = () => {
+    setState({totalMoves: state.totalMoves + 1})
+  }
+
+  const moveRight = () => {
+    removeItemFromFrontOfArray()
+    addToFrontOfArray()
+    incrementTotalMoves()
+  }
+
+  const moveLeft = () => {
+    removeItemFromFrontOfArray()
+    addToBackOfArray()
+    incrementTotalMoves()
+  }
+
+  const moveUp = () => {
+    moveBUpWithPushAndShift()
+    incrementTotalMoves() 
+  } 
+
+  const moveDown = () => {
+    moveBDownWithUnshiftAndPop()
+    incrementTotalMoves()
+   } 
+
+  const handleRightClick = () => {
+    const newMessage = "You can't go right"
+    if (state.board.indexOf('B', 0) === 2) {return setState({...state, message: newMessage})}
+    else if(state.board.indexOf('B', 0) === 5) {return setState({...state, message: newMessage})}
+    else if(state.board.indexOf('B', 0) === 8) {return setState({...state, message: newMessage})}
+    else {return moveRight()}
+  }
+
+  const handleLeftClick = () => {
+    const newMessage = "You can't go left"
+    if (state.board.indexOf('B', 0) === 0) {return setState({...state, message: newMessage})}
+    else if(state.board.indexOf('B', 0) === 3) {return setState({...state, message: newMessage})}
+    else if(state.board.indexOf('B', 0) === 6) {return setState({...state, message: newMessage})}
+    else {return moveLeft()}
+  }
+
+  const handleUpClick = () => {
+    const newMessage = "You can't go up"
+    if (state.board.indexOf('B', 0) === 0) {return setState({...state, message: newMessage})}
+    else if(state.board.indexOf('B', 0) === 1) {return setState({...state, message: newMessage})}
+    else if(state.board.indexOf('B', 0) === 2) {return setState({...state, message: newMessage})}
+    else {return moveUp()}
+  }
+
+  const handleDownClick = () => {
+    const newMessage = "You can't go down"
+    if (state.board.indexOf('B', 0) === 6) {return setState({...state, message: newMessage})}
+    else if(state.board.indexOf('B', 0) === 7) {return setState({...state, message: newMessage})}
+    else if(state.board.indexOf('B', 0) === 8) {return setState({...state, message: newMessage})}
+    else {return moveDown()}
+  }
+
+  const onChangeOfEmail = (evt) => {
+    const { value } = evt.target
+    setState({ ...state, emailInput: value })
+  }
+
+  const postNewEmail= () => {
+    axios.post(PostURL,
+       {x: state.xCoordinate,
+        y: state.yCoordinate,
+        steps: state.totalMoves,
+        email: state.emailInput })
+        .then(res => {
+          console.log(res.data)
+          setState({...state, message: res.data.message})
+        })
+        .catch(err => {
+          console.log(err)
+        })
+  } 
+
+  const onSubmit = (evt) => {
+    evt.preventDefault()
+    postNewEmail()
+    setState({...state, emailInput: ''})
+  } 
+
 
   
   return (
     <div id="wrapper" className={props.className}>
-      <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
-      </div>
+ <div className="info">
+          <h3 id="coordinates">{`Coordinates ${getXY(indexOfB)}`}</h3>
+          <h3 id="steps">You moved {state.totalMoves} times</h3>
+        </div>
+
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+
+          {
+            state.board.map((val, idx) => {
+              return (
+                <div key={idx} className={`square${val ? ' active' : ''}`}>{val}</div>
+              ) 
+          })}
+
       </div>
-      <div className="info">
-        <h3 id="message"></h3>
-      </div>
-      <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
-      </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
-      </form>
+
+        <div className="info">
+          <h3 id="message">{state.message}</h3>
+        </div>
+        <div id="keypad">
+          <button onClick={handleLeftClick} id="left">LEFT</button>
+          <button onClick={handleUpClick} id="up">UP</button>
+          <button onClick={handleRightClick} id="right">RIGHT</button> 
+          <button onClick={handleDownClick} id="down">DOWN</button>
+          <button onClick={reset} id="reset">reset</button>
+        </div>
+        <form onSubmit={onSubmit}>
+          <input value={state.emailInput} onChange={onChangeOfEmail} id="email" type="email" placeholder="type email"></input>  
+          <input id="submit" type="submit"></input>
+        </form>
     </div>
   )
 }
