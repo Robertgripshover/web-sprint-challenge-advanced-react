c2f68352a1254942e4329956ad7af03216a93e0c
Robert Gripshover <robertgrips@gmail.com>
2022-10-06 16:41:20 -0400

    working on the tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -4,36 +4,17 @@
 //   - From inside the test file, import a component of your choosing, either `AppClass.js` or `AppFunctional.js`.
 //   - Test that the visible texts in headings, buttons, links... render on the screen.
 //   - Test that typing on the input results in its value changing to the entered text.
-
 import React from 'react';
-import {render, screen} from "@testing-library/react";
+import {render} from "@testing-library/react";
+// import userEvent from '@testing-library/user-event'
 import AppClass from './AppClass';
 
-test('sanity', () => {
-  expect(true).toBe(false)
-})
-
-test('Render without errors', () => {
-  render(<AppClass />)
-})
-
-// test('When app mounts, header Welcome to the GRID exists on the screen', () => {
-//   //Arrage, -->>
-//   // that is getting the AppClass rendered to the screen
-//   //for it to be able to be tested
-//   render(<AppClass />)
-
-//   //Act, -->>
-//   const leftButton = screen.getByTestId('left')
-//   //if we were to do queryByText it would move on to 
-
-//   //Assert -->>
-//   expect(leftButton).toBeInTheDocument()
-//   expect(leftButton).toHaveTextConent(/left/i)
 
 
+// test('sanity', () => {
+//   expect(true).toBe(false)
 // })
 
-
-
-
+test('renders without errors', () => {
+  render(<AppClass />)
+})
